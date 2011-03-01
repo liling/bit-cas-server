6c674ea0d225c6c90f6fe5f488c1cc9b5dc4bb4b
Ling Li <lilingv@gmail.com>
2011-03-01 20:14:45 +0800

    实现检查用户是否已经设定邮箱和手机的功能

--- src/main/java/org/jasig/cas/web/flow/AccountSafetyCheckAction.java
+++ src/main/java/org/jasig/cas/web/flow/AccountSafetyCheckAction.java
@@ -1,13 +1,76 @@
 package org.jasig.cas.web.flow;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.authentication.principal.CredentialsToPrincipalResolver;
+import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.ticket.TicketGrantingTicket;
+import org.jasig.cas.ticket.registry.TicketRegistry;
+import org.jasig.cas.web.support.WebUtils;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 public class AccountSafetyCheckAction extends AbstractAction {
+	
+	private SimpleJdbcTemplate jdbcTemplate;
+	private DataSource dataSource;
+	private TicketRegistry ticketRegistry;
+	
+	protected Principal getPrincipal(RequestContext context) {
+		String tgtid = WebUtils.getTicketGrantingTicketId(context);
+		TicketGrantingTicket tgt = (TicketGrantingTicket) ticketRegistry.getTicket(tgtid);
+		return tgt.getAuthentication().getPrincipal();
+	}
+	
+	protected Event doExecute(RequestContext context) 
+	throws Exception {
+		if (jdbcTemplate == null) 
+			jdbcTemplate = new SimpleJdbcTemplate(dataSource);
+		String rst = "safe";
+		
+		Principal principal = this.getPrincipal(context);
+		System.out.println("Username is " + principal.getId());
+		try {
+			Map<String,Object> row = jdbcTemplate.queryForMap(
+				"SELECT * FROM pf_users WHERE username=?", principal.getId());
+			if (row != null) {
+				context.getFlowScope().put("hasSafeMail", new Boolean(
+						row.get("mail") != null && 
+						((String) row.get("mail")).length() > 0));
+				context.getFlowScope().put("hasSafeMobile", new Boolean(
+						row.get("mobile") != null && 
+						((String) row.get("mobile")).length() > 0));
+				if (!context.getFlowScope().getBoolean("hasSafeMail") ||
+					!context.getFlowScope().getBoolean("hasSafeMobile"))
+				{
+					rst = "unsafe";
+				}
+			} else {
+				rst = "unsafe";
+			}
+		} catch (EmptyResultDataAccessException e) {
+			rst = "unsafe";
+		}
+		return result(rst);
+	}
 
-    protected Event doExecute(RequestContext context) throws Exception {
-        return result("unsafe");
-    }
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
+	public void setJdbcTemplate(SimpleJdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public void setTicketRegistry(TicketRegistry ticketRegistry) {
+		this.ticketRegistry = ticketRegistry;
+	}
+	
 }
